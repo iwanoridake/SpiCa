ee07c324e3cbf375973409ed37111403d4e6a8f7
iwanoridake <riwano.ikeda@fj.ics.keio.ac.jp>
2022-05-05 10:00:40 +0900

    戻した

---
+++ Assets/Main/Scripts/Shader/Offset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Offset : MonoBehaviour
+{
+    Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = gameObject.GetComponent<Renderer>().material;
+        material.SetTextureOffset("_MainTex", new Vector2(-15000, -15000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
